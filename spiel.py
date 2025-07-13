7e1fd34e70c0965f5bb9212643ba66c3d4f802c2
Nicho Kuschmann <nicholaskuschmann@gmail.com>
2025-07-13 16:15:41 +0200

    nextes Level

--- spiel.py
+++ spiel.py
@@ -1,21 +1,32 @@
 import random
-geheime_zahl = random.randint(1,100)
-print("Lass uns ein Spiel Spielen")
-print("Ich denke an eine Zahl zwischen 1 und 100")
-versuche = 0
-while True:
-    eingabe = input("Rate die Zahl: ")
+def spiel_starten():
+    geheime_zahl = random.randint(1,100)
+    print("Lass uns ein Spiel Spielen")
+    print("Ich denke an eine Zahl zwischen 1 und 100")
+    versuche = 0
+    while True:
+        eingabe = input("Rate die Zahl: ")
 
-    if not eingabe.isdigit():
-        print("Probiers mit ner Zahl, Daggl!")
+        if not eingabe.isdigit():
+            print("Probiers mit ner Zahl, Daggl!")
+            continue
+        tipp = int(eingabe)
+        versuche +=1
+        if tipp < geheime_zahl:
+         print("Das war zu klein!")
+        elif tipp > geheime_zahl:
+         print("probiers mal kleiner")
+        else:
+            print(f"Super, die Zahl war {geheime_zahl}.")
+            print(f"du hast {versuche} Versuche gebraucht.")
+            break
+while True:
+    spiel_starten()
+    nochmal = input("Magst du nochma? (j/n): ")
+    if nochmal.lower() == "j":
         continue
-    tipp = int(eingabe)
-    versuche +=1
-    if tipp < geheime_zahl:
-        print("Das war zu klein!")
-    elif tipp > geheime_zahl:
-        print("probiers mal kleiner")
-    else:
-        print(f"Super, die Zahl war {geheime_zahl}.")
-        print(f"du hast {versuche} Versuche gebraucht.")
+    elif nochmal.lower() == "n":
+        print("Danke fürs Dabeisein, bis bald")
         break
+    else:
+       nochmal = input("Wir können nur mit nem Ja oder Nein weitermachen (j/n)")
